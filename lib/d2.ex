64a52aa880b7e4748b8cb3dcab9be23f107870dd
Angelo Moreira <angelo.moreira@gmail.com>
2022-12-08 23:53:47 +0000

    day 2 part 2

--- lib/d2.ex
+++ lib/d2.ex
@@ -21,9 +21,31 @@ defmodule D2 do
     "C Z" => @scissors + @draw
   }
 
+  @rankings2 %{
+    "A X" => @scissors + @lost,
+    "A Y" => @rock + @draw,
+    "A Z" => @paper + @win,
+    "B X" => @rock + @lost,
+    "B Y" => @paper + @draw,
+    "B Z" => @scissors + @win,
+    "C X" => @paper + @lost,
+    "C Y" => @scissors + @draw,
+    "C Z" => @rock + @win
+  }
+
   Enum.each(@rankings, fn {move, points} ->
     def match_move(unquote(move)), do: unquote(points)
   end)
+
+  Enum.each(@rankings2, fn {move, points} ->
+    def match_move2(unquote(move)), do: unquote(points)
+  end)
+
+  def to_list(stream) do
+    stream
+    |> Stream.map(&String.trim/1)
+    |> Enum.take_every(1)
+  end
 
   def part1(file) do
     file
@@ -32,10 +54,22 @@ defmodule D2 do
       trim: true,
       modes: [:raw, :raw_ahead, :trim_bom]
     )
-    |> Stream.map(&String.trim/1)
-    |> Enum.take_every(1)
+    |> to_list()
     |> Enum.reduce(0, fn lol_tom, acc ->
       acc + match_move(lol_tom)
+    end)
+  end
+
+  def part2(file) do
+    file
+    |> File.stream!(
+      line_or_bytes: :line,
+      trim: true,
+      modes: [:raw, :raw_ahead, :trim_bom]
+    )
+    |> to_list()
+    |> Enum.reduce(0, fn lol_tom, acc ->
+      acc + match_move2(lol_tom)
     end)
   end
 end
--- test/d2_test.exs
+++ test/d2_test.exs
@@ -1,10 +1,15 @@
 defmodule D2Test do
   @moduledoc false
   use ExUnit.Case
-  import D2, only: [part1: 1]
+  import D2, only: [part1: 1, part2: 1]
 
   @tag :d2_p1
-  test "should produce correct day1 part 1 correct output" do
+  test "should produce correct day2 part 1 correct output" do
     assert part1("#{__DIR__}/d2_input.txt") == 15
+  end
+
+  @tag :d2_p2
+  test "should produce correct day2 part 2 correct output" do
+    assert part2("#{__DIR__}/d2_input.txt") == 12
   end
 end
